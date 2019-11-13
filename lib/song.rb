ce0c91a68e2a92b11ad715ef974296a418f6e7c3
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-13 21:13:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,15 +3,17 @@ class Song
 
     @@count = 0
 
+
   def initialize(name,artist,genre)
     @name = name
-    @artist = artist
-    @genre = genre
+    @@artist = artist
+    @@genre = genre
     @@count += 1
   end
 
   def count
     @@count
   end
+
 
 end
