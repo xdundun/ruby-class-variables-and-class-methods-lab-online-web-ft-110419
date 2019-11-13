d61781f6fca5f5db128ef506f2e668b732c80894
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-13 21:28:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
   attr_accessor :name, :artist, :genre
   #attr_reader :count
     @@count = 0
-    @@artists
+    @@artists = []
     @@genres = []
 
   def initialize(name,artist,genre)
